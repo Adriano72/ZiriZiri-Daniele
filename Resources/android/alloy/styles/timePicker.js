aa729b6899a3fee7ecdda2098a135eca27e63ead
Adriano Massi <adrianomassi@gmail.com>
2014-05-18 12:59:27 +0200

    zxcz

--- Resources/android/alloy/styles/timePicker.js
+++ Resources/android/alloy/styles/timePicker.js
@@ -1,1 +1,1 @@
-module.exports = [{"isId":true,"priority":100000.0121,"key":"pkrTime","style":{type:Ti.UI.PICKER_TYPE_TIME,right:5,left:5,width:Ti.UI.FILL,height:Ti.UI.SIZE,}}];
+module.exports = [{"isId":true,"priority":100000.0123,"key":"pkrTime","style":{type:Ti.UI.PICKER_TYPE_TIME,right:5,left:5,width:Ti.UI.FILL,height:Ti.UI.SIZE,}}];
